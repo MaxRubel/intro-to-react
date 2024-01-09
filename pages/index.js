6ff4effac3b3b9d239e0b706f89143c5b2740f1f
MaxRubel <maxrubel1@gmail.com>
2024-01-09 14:51:51 -0600

    counter working

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,43 @@
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 // import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
-function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+import { useState } from 'react';
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+function Home() {
+  const [value, setValue] = useState(0);
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
+    <>
+      <h1>Counter App</h1>
+      <h2>{value}</h2>
+      <button
+        type="button"
+        key="inc"
+        onClick={() => {
+          setValue(value + 1);
+        }}
+        className="button"
+      >
+        Increment
+      </button>
+      <button
+        type="button"
+        key="dec"
+        onClick={() => {
+          setValue(value - 1);
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        type="button"
+        key="reset"
+        onClick={() => {
+          setValue(0);
+        }}
+      >
+        Reset
+      </button>
+    </>
   );
 }
 
